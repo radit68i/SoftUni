02e170e6262130fd0a77b8c609dc21cf076dc68b
radit68i <30549900+radit68i@users.noreply.github.com>
2020-09-17 14:10:43 +0300

    01. Basic Syntax, Conditional Statements and Loops - Lab

---
+++ C# Fundamentals - september 2020/lab/01. Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{num} X {multiplier} = {num * multiplier}");
+            }
+
+            for (int i = multiplier; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} X {i} = {num * i}");
+            }
+        }
+    }
+}
