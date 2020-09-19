16d4be5253162f17d4132c260fe3760516097c32
radit68i <30549900+radit68i@users.noreply.github.com>
2020-09-19 19:44:04 +0300

    initial commit 02. CSharp-Fundamentals-Data-Types-and-Variables-Exercise

---
+++ C# Fundamentals - september 2020/exercise/02. CSharp-Fundamentals-Data-Types-and-Variables-Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sumOfDigits = 0;
+            while(num != 0)
+            {
+                int digit = num % 10;
+                num /= 10;
+                sumOfDigits += digit;
+            }
+
+            Console.WriteLine(sumOfDigits);
+        }
+    }
+}
